8264ce84c56bf2b4c5194ba0d0c8096762861ef7
AlexGaCM <agajer@conversionmaker.com>
2022-11-08 09:56:07 +0100

    add dialog feature

---
+++ src/components/DialogButton.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import Translate from '../components/Translation'
+
+export default function DialogButton(props) {
+
+  const [open, setOpen] = useState(false)
+  const [color, setColor] = useState(" text-black ")
+  const [previewColor, setPreviewColor] = useState(" text-black ")
+  const [title, setTitle] = useState("Title")
+  const [content, setContent] = useState(title)
+
+  function handleChange(event) {
+    setContent(event.target.value)
+  }
+
+  function handleClose() {
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <div className='laptop:ml-[300px] desktop:ml-[340px]'>
+        <h1 className={'text-3xl mt-16 ' + color}>
+          {title}
+        </h1>
+        <button className={props.buttonClassName} onClick={() => setOpen(!open)}>
+          <Translate placeholder='dialog_open' />
+        </button>
+      </div>
+      <div className={ open ? 'absolute h-screen w-screen bg-slate-900 opacity-50 top-0' : 'hidden'}></div>
+      <div className={ open ? 'absolute h-screen w-screen top-0 backdrop-blur-sm' : 'hidden'}>
+        <div className='flex desktop:mt-[15%] laptop:mt-[10%] justify-center place-content-center'>
+          <div className={ open ? props.dialogClassName : 'hidden'}>
+            <div className="bg-blue-500 h-12 w-full">
+              <button className='float-right h-full w-12 text-white' onClick={handleClose}>
+                X
+              </button>
+            </div>
+            <form className='text-center'>
+              <textarea
+                className='mt-12 resize-none text-center w-4/6 h-32 rounded border-2 border-blue-500'
+                placeholder='Enter Text'
+                onChange={handleChange}
+              >{title}</textarea>
+            </form>
+            <div className='flex'>
+              <div className='ml-8 mt-8 gap-x-4 flex'>
+                <button className='bg-blue-600 hover:bg-blue-500 w-28 h-28 rounded-md' onClick={() => setPreviewColor(" text-blue-500 ")}>
+                </button>
+                <button className='bg-red-600 hover:bg-red-500 w-28 h-28 rounded-md' onClick={() => setPreviewColor(" text-red-600 ")}>
+                </button>
+                <button className='bg-emerald-500 hover:bg-emerald-400 w-28 h-28 rounded-md' onClick={() => setPreviewColor(" text-emerald-500 ")}>
+                </button>
+                <button className='bg-pink-400 hover:bg-pink-300 w-28 h-28 rounded-md' onClick={() => setPreviewColor(" text-pink-400 ")}>
+                </button>
+              </div>
+              <div className='ml-12 mt-4'>
+                <div>
+                  <Translate placeholder='dialog_preview' />
+                </div>
+                <div className={'bg-white desktop:w-96 laptop:w-80 desktop:h-30 laptop:h-28 rounded-md px-2 py-1 resize-none text-3xl overflow-scroll text-center' + previewColor}>
+                  {content}
+                </div>
+              </div>
+            </div>
+            <div className='float-right flex gap-x-2 mt-10 mr-6'>
+              <button
+                className='border-slate-400 hover:bg-slate-400 hover:text-white border-2 rounded-md h-10 w-24'
+                onClick={handleClose}
+              >
+                <Translate placeholder='dialog_close' />
+              </button>
+              <button
+                className='bg-emerald-500 hover:bg-emerald-400 text-white rounded-md h-10 w-24'
+                onClick={() => {
+                  setTitle(content);
+                  setColor(previewColor);
+                  handleClose()
+                }}
+              >
+                <Translate placeholder='dialog_submit' />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,23 @@
 import Navigation from '../components/Navigation'
 import Topbar from '../components/Topbar'
 import Translate from "../components/Translation";
+import DialogButton from '../components/DialogButton'
 
 export default function Home() {
 
   return (
     <>
-      <div>
-        <Navigation />
-        <Topbar />
-        <div className='mt-[40px] laptop:ml-[300px] desktop:ml-[340px]'>
-          <h1 className='text-3xl'>
-            <Translate placeholder='module_name_dashboard' />
-          </h1>
-        </div>
+      <Navigation />
+      <Topbar />
+      <div className='mt-[40px] laptop:ml-[300px] desktop:ml-[340px]'>
+        <h1 className='text-3xl'>
+          <Translate placeholder='module_name_dashboard' />
+        </h1>
       </div>
+      <DialogButton
+        buttonClassName='mt-8 bg-blue-500 hover:bg-blue-400 text-white rounded py-1 px-2'
+        dialogClassName='absolute w-[1020px] h-[480px] m-1/2 bg-slate-200 drop-shadow-lg'
+      />
     </>
   )
 }
