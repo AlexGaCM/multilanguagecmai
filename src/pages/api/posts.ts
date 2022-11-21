77eb5c805c225f336a773b33a1b82e4d779b650b
AlexGaCM <agajer@conversionmaker.com>
2022-11-21 10:43:14 +0100

    database call returns data in console

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -6,15 +6,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const dbClient = await clientPromise;
   const db = dbClient.db('sample_supplies')
   const collection = db.collection('sales')
-  const data1 = await collection.findOne({storeLocation: "Denver"})
-  const data2 = await collection.findOne({storeLocation: "Seattle"})
-  // const data = await collection.find({storeLocation: "Denver"})
+  const data = await collection.find({storeLocation: "San Diego", couponUsed: true, purchaseMethod: "Online"}).toArray()
 
-  const myJSON = {
-    Denver: {data1},
-    Seattle: {data2}
-  }
-
-  res.status(200).json(myJSON)
+  res.status(200).json(data)
 }
 
